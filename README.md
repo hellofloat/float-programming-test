ce3c52d835dbbfe0588e22168fbac9ee534143e0
Andy Burke <aburke@bitflood.org>
2016-06-15 17:40:21 -0700

    Add some more basic tests

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ To submit this test:
 
 1. Fork this repository to your personal github account
 2. Complete the implementation of each test in the src/ folder
-3. Ensure the included tests pass
+3. Ensure the included tests pass (run: npm test)
 4. Create an issue with the url of your fork
 
 ## Unit Tests
--- test/balancedParentheses.js
+++ test/balancedParentheses.js
@@ -17,3 +17,21 @@ test( 'BALANCED PARENTHESES: returns true for balanced string', t => {
     t.equal( result, true, 'result is correct' );
     t.end();
 } );
+
+test( 'BALANCED PARENTHESES: returns false for empty string', t => {
+    const input = '';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false for string with no parentheses', t => {
+    const input = 'x + y';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
